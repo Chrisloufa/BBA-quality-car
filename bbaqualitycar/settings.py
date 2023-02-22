97172295c38c34a9fa0f7956428500aabc84165e
Chrisloufa <89994465+Chrisloufa@users.noreply.github.com>
2023-02-22 13:26:20 +0000

    changes ti footer

--- bbaqualitycar/settings.py
+++ bbaqualitycar/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django_countries',
     'crispy_forms',
+    "crispy_bootstrap5",
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -165,3 +166,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
