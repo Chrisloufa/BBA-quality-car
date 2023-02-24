6ce024e318e37e7837c5e4c095364147fbb42865
Chrisloufa <89994465+Chrisloufa@users.noreply.github.com>
2023-02-24 14:35:18 +0000

    added logo

--- bbaqualitycar/urls.py
+++ bbaqualitycar/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +25,5 @@ urlpatterns = [
     path('', include("members.urls")),
     path('', include("contact.urls")),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
